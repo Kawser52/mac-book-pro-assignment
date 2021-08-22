76518d283c56bb6c13781c7452e6b2a788958d21
Soriful Islam Kawser <shoriful521399@gmail.com>
2021-08-22 21:10:38 +0600

    grandtotal er kaj ses;

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
-const eigthtGb= document.getElementById('eight-gb');
-const sixteenGb= document.getElementById('sixteen-gb');
 const freeCharge = document.getElementById ('free-shipping');
 const sixteenGbValue=document.getElementById('sixteenGb');
 const bestPrice = document.getElementById('fixedPrice');
 const totalPrice= document.getElementById ('totalPrice');
+const grandTotalAmount = document.getElementById('totalGrandPrice');
+;
+function eighGBandDelivery(params) {
+    
+}
 
-
-eigthtGb.addEventListener('click',function(){
+document.getElementById('eight-gb').addEventListener('click',function(){
     freeCharge.innerText= '0';
     updateTotalPrice();
 })
-sixteenGb.addEventListener('click',function(){
+document.getElementById('sixteen-gb').addEventListener('click',function(){
     freeCharge.innerText= '180';
     updateTotalPrice();
 })
@@ -28,15 +30,26 @@ document.getElementById('ssd1Tb').addEventListener('click',function(){
     extraStorage.innerText= '180';
     updateTotalPrice();
 })
+document.getElementById('freeCharge').addEventListener('click',function(){
+    freePrice.innerText = '0';
+    updateTotalPrice();
+})
+document.getElementById('Takalagbe').addEventListener('click',function(){
+    freePrice.innerText = '20';
+    updateTotalPrice();
+})
 
 
 function updateTotalPrice(){
  const productPriceValue= parseInt(freeCharge.innerText);
  const bestAmount = parseInt(bestPrice.innerText);
  const storagePrice = parseInt(extraStorage.innerText);
- const totalAmount= productPriceValue+bestAmount+storagePrice;
+ const delvieryCharge= parseInt(freePrice.innerText);
+ const totalAmount= productPriceValue+bestAmount+storagePrice+delvieryCharge;
  totalPrice.innerText=totalAmount;
+ grandTotalAmount.innerText = totalAmount;
 }
+
 
 
 
